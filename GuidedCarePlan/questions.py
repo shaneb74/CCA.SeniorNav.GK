caeb11e4ab87e5c23d03c02379e0defd029cc811
shaneb74 <shaneb@conciergecareadvisors.com>
2025-09-30 18:28:47 -0500

    Delete GuidedCarePlan directory

--- GuidedCarePlan/questions.py
+++
@@ -1,4 +0,0 @@
-QUESTIONS = [
-    ("cognition_level", "How would you rate your memory and thinking in daily life?", 
-     ["Sharp", "Sometimes forgetful", "Frequent memory issues", "Serious confusion"], None),
-]
--- GuidedCarePlan/view.py
+++
@@ -1,4 +0,0 @@
-from .engine import run_flow
-
-def render():
-    run_flow()
