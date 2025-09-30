105c14ea984270dc6c15e5c39838f03712ac559c
shaneb74 <shaneb@conciergecareadvisors.com>
2025-09-29 21:23:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import logic  # local module with the app's core logic
 
-# ======================
-# Global Visual Treatment
-# ======================
 st.set_page_config(page_title="Senior Navigator", page_icon="🧭", layout="wide")
 
 st.markdown('''
